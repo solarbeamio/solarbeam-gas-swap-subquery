f9659fc14e278329e77d36e79a2896b4124e2103
carrion9 <snadec@hotmail.com>
2022-06-03 07:11:53 +0300

    decode function

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -5,8 +5,35 @@ import {Transaction} from "../types";
 // Setup types from ABI
 // type TransferEventArgs = [string, string, BigNumber] & { from: string; to: string; value: BigNumber; };
 // type ApproveCallArgs = [string, BigNumber] & { _spender: string; _value: BigNumber; }
-type EventArgs = [string, string, any] & { userAddress: string; relayerAddress: string, functionSignature: any };
+type EventArgs = [string, string, any] & { userAddress: string; relayerAddress: string, functionSignature: string };
 
+function decodeFunctionSignature(functionData: string){
+    const PREFIX = 2
+    const FUNCTION = 8
+    const WORD = 64
+    const ADDRESS_OFFSET = 24
+    const SWAP_OP = '627dd56a'
+
+    const functionOpcode = functionData.substring(PREFIX, PREFIX + FUNCTION)
+
+    if (functionOpcode.localeCompare(SWAP_OP) == 0) {
+
+        const data = functionData.substring(PREFIX + FUNCTION)
+        const amount = BigInt('0x' + data.substring(2 * WORD, 3 * WORD))
+        const amountOutMin = BigInt('0x' + data.substring(3 * WORD, 4 * WORD))
+        const user = '0x' + data.substring(5 * WORD + ADDRESS_OFFSET, 6 * WORD)
+        const token = '0x' + data.substring(11 * WORD + ADDRESS_OFFSET, 12 * WORD)
+
+        return {
+            amount,
+            amountOutMin,
+            user,
+            token
+        }
+    }
+
+    return null
+}
 
 export async function handleMetaTransactionEvent(event: MoonbeamEvent<EventArgs>): Promise<void> {
     let transaction = new Transaction(event.transactionHash);
@@ -15,12 +42,12 @@ export async function handleMetaTransactionEvent(event: MoonbeamEvent<EventArgs>
     let hexSignature = event.args.functionSignature;
     logger.info(`transaction: ${{...transaction}}`);
     logger.info('=================================================================================');
-    logger.info(`transaction.userAddress: ${transaction.userAddress.toString()}`);
+    logger.info(`transaction.userAddress: ${transaction.userAddress}`);
     logger.info('=================================================================================');
     logger.info(`hexSignature: ${hexSignature.toString()}`);
     logger.info('=================================================================================');
-    let takis = utils.defaultAbiCoder.decode(['uint256'], hexSignature);
-    logger.info(`takis: ${takis}`)
+    // let takis = utils.defaultAbiCoder.decode(['uint256'], hexSignature);
+    // logger.info(`takis: ${takis}`)
     await transaction.save();
 }
 
